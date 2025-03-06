540c880d145e3bdd85888225c6981029e090c538
Marwan Rouissi <rouissi-marwan@hotmail.fr>
2025-03-06 15:13:43 +0100

    added seeds from api

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,3 +7,27 @@
 #   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
+
+require 'open-uri'
+require 'json'
+
+url = 'https://tmdb.lewagon.com/movie/top_rated'
+movies_serialized = URI.parse(url).read
+movies = JSON.parse(movies_serialized)['results']
+img_base = 'https://image.tmdb.org/t/p/w500'
+
+puts 'Cleaning DB...'
+Movie.destroy_all
+
+puts 'Creating movies...'
+movies.first(10).each do |movie|
+  movie = {
+    title: movie['title'],
+    overview: movie['overview'],
+    poster_url: img_base + movie['poster_path'],
+    rating: movie['vote_average'].round(1)
+  }
+  Movie.create(movie)
+end
+
+puts "Finished! Created #{Movie.count} movies."
